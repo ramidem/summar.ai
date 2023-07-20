f0f03839575293f7186a87df864dc52812888c42
Med Aduh <im.ramidem@gmail.com>
2023-07-20 23:15:01 +0800

    figlet!

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
+import figlet from "figlet";
+
 const server = Bun.serve({
   port: 3000,
   fetch(req) {
-    return new Response("Hello, Bun 🍞!");
+    return new Response("Hello, Bun!");
   },
 });
 
-console.log(`listening on ${server.port}!`);
+console.log(figlet.textSync("Summa.ai!"), "\n", `Listening on ${server.port}!`);
